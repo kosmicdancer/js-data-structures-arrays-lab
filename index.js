91d8bb78b6d86d417fc848c38ab0b17efdfecff1
Kyoko <kyokoconnect@gmail.com>
2021-04-06 23:32:27 -0400

    1st commit, lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(Ralph) {drivers.push (Ralph)}
+
+function destructivelyPrependDriver(Bob) {drivers.unshift (Bob)}
+
+function destructivelyRemoveLastDriver(Garfield) {drivers.pop (Garfield)}
+
+function destructivelyRemoveFirstDriver(Milo) {drivers.shift (Milo)}
+
+function appendDriver(Broom) {
+    return [...drivers, Broom] }
+
+function prependDriver(Arnold) {
+    return [Arnold, ...drivers]}
+
+function removeFirstDriver() {
+    return drivers.slice (1)}
+
+function removeLastDriver() {
+    return drivers.slice (0, drivers.length - 1)}
